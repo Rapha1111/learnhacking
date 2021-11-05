9205ce71a70b994d0476c3cbdfc4ba51aa540bd9
Rapha1111 <75113476+Rapha1111@users.noreply.github.com>
2021-11-05 19:34:17 +0100

    Add files via upload

---
+++ mission/1/script.js
@@ -0,0 +1,8 @@
+function Check(){
+	var code=document.code.code.value;
+	if (code=="GTROUVE") {
+	    alert("Bravo, vous avez réussi cette mission retournez au menu et choisissez la mission suivante");
+	} else { 
+	    alert("Mauvais mot de passe"); 
+	}
+}
