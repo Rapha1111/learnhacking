63b521aaba4b9d7c7fc20dc7735e2a8a097106dc
Rapha1111 <75113476+Rapha1111@users.noreply.github.com>
2021-11-05 20:49:02 +0100

    Add files via upload

---
+++ mission/4/script.js
@@ -0,0 +1,8 @@
+function Check(){
+	var code=document.code.code.value;
+	if (code=="JeSuisInvisible") {
+	    alert("Bravo, vous avez réussi cette mission retournez au menu et choisissez la mission suivante");
+	} else { 
+	    alert("Mauvais mot de passe"); 
+	}
+}
